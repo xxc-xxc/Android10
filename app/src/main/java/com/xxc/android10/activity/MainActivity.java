832316f0480d998976aa426cc28b604edeaab19e
xuxc <xxch7219@163.com>
2020-08-24 16:24:51 +0800

    Android10新特性首次提交

---
+++ app/src/main/java/com/xxc/android10/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xxc.android10.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xxc.android10.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
